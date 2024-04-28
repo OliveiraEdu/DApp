5b4036575c5b01a6bcbafb035b89087bcc550c37
OliveiraEdu <eduardocostaoliveira@gmail.com>
2024-04-28 18:27:57 -0300

    updating repo

--- convert.py
+++ convert.py
@@ -1,12 +1,9 @@
 from Crypto.Hash import keccak
 k = keccak.new(digest_bits=256)
-k.update(b'admin@test')
+k.update(1000)
 
 print (k.hexdigest())
 
 print ((k.hexdigest()[24:64]).zfill(64))
 
 
-k.update(b'1000')
-
-print (k.hexdigest())
