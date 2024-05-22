d78aeffd7e25cf2e6406dd1ede0ccfc7116ca424
OliveiraEdu <eduardocostaoliveira@gmail.com>
2024-05-22 18:38:00 -0300

    flash updating repo

---
+++ docker_restart.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Stop the containers if they are running
+docker stop some-postgres
+docker stop iroha
+
+# Remove all unused data
+docker system prune -f
+
+# Create a new Docker network
+docker network create iroha-network
+
+# Remove the old volume and create a new one
+docker volume rm blockstore
+docker volume create blockstore
+
+# Run the PostgreSQL container
+docker run --name some-postgres \
+    -e POSTGRES_USER=postgres \
+    -e POSTGRES_PASSWORD=mysecretpassword \
+    -p 5432:5432 \
+    --network=iroha-network \
+    -d postgres:9.5 \
+    -c 'max_prepared_transactions=100'
+
+
+# Run the Iroha container
+cd /Git
+docker run --name iroha \
+    --rm \
+    -d \
+    -p 50051:50051 \
+    -p 7001:7001 \
+    -v $(pwd)/iroha/example:/opt/iroha_data \
+    -v blockstore:/tmp/block_store \
+    --network=iroha-network \
+    -e KEY='node0' \
+    hyperledger/iroha-burrow:1.5.0-debug
