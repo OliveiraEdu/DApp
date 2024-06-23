ea0e770386d3a28326a99b01adb162cb9208a7d6
OliveiraEdu <eduardocostaoliveira@gmail.com>
2024-06-23 16:32:04 -0300

    updating

--- burrow_integration/detailv2.py
+++ burrow_integration/detailv2.py
@@ -47,7 +47,7 @@ csv_file_path = 'accounts.csv'
 accounts = read_accounts_from_csv(csv_file_path)
 
 # Use the [n] account from the CSV for the example
-account = accounts[8]
+account = accounts[7]
 
 @integration_helpers.trace
 def create_contract():
